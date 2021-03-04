695f811c791a5c55f089f404fb8247d6a4678279
Jhon Sebastian <43323559+Segaretsu@users.noreply.github.com>
2021-03-04 09:08:33 -0500

    Se actualizan términos y condiciones tomando como referencia los de google analitycs - 04/03/2021, 09:08 a.m.
    - Se actualizan términos y condiciones tomando como referencia los de google analitycs - 04/03/2021, 09:08 a.m.
    - Analíticos de google: https://analytics.google.com/analytics/web/provision/?authuser=1

--- src/constants/TerminosAndCondiciones.js
+++ src/constants/TerminosAndCondiciones.js
@@ -1,14 +1,40 @@
 module.exports = Object.freeze(
     [
         {
-            titulo: 'Información que es recogida',
+            titulo: 'Condiciones del aplicativo, servicios y sensores de Controlsep',
+            descripcion: `Estas Condiciones son vinculantes para Controlsep y la entidad que celebra este Acuerdo (el "Usuario").
+            Este Acuerdo regula el uso por parte del Usuario de los Servicios, sensores y aplicaciones de Controlsep.
+            AL COMPLETAR EL PROCESO DE REGISTRO O AL USAR EL SERVICIO O SENSORES, MANIFIESTA QUE HA REVISADO Y ACEPTA ESTE ACUERDO,
+            Y QUE ESTÁ AUTORIZADO PARA ACTUAR EN NOMBRE DEL PROPIETARIO DE ESTA CUENTA Y PARA VINCULARLE CON RESPECTO AL PRESENTE ACUERDO.
+            En virtud de lo anterior, las partes acuerdan lo siguiente:`
+        },
+        {
+            titulo: '01. Definiciones',
+            descripcion: `Por "Información Confidencial" se entiende cualquier tipo de información privada y cualquier otra información
+            que una de las partes dé a conocer a la otra por escrito y que haya sido marcada como confidencial o que esa parte haya
+            revelado oralmente y que, en un plazo de cinco días laborables desde ese momento, haya documentado por escrito y marcado como confidencial.
+            Sin embargo, no se considera Información Confidencial aquella información que sea de dominio público, o que se haya divulgado públicamente,
+            que ya estuviera en posesión de la parte receptora antes de que fuera divulgada por una de las partes o a la que la parte receptora hubiera
+            llegado de forma independiente y sin hacer uso de ningún tipo de Información Confidencial.
+               
+            "Política de Privacidad" se refiere a la política de privacidad de una Propiedad.
+
+            "Tercero" se refiere a cualquier tercero (i) a quien el Usuario facilite el acceso a Su Cuenta o (ii)
+            en cuyo nombre el Usuario utilice el Servicio para recoger información.
+
+            "Usuarios" significa usuarios y/o visitantes de las Propiedades del Usuario.
+
+            Las palabras "incluye", "incluido" y sus derivados se refieren a "incluido, pero sin limitarse a ello".`
+        },
+        {
+            titulo: '02. Información que es recogida',
             descripcion: `Nuestro sitio web podrá recoger información personal por ejemplo:
             Nombre,  información de contacto como  su dirección de correo electrónica e información demográfica.
              Así mismo cuando sea necesario podrá ser requerida información específica para procesar algún pedido
             o realizar una entrega o facturación.`
         },
         {
-            titulo: 'Uso de la información recogida',
+            titulo: '03. Uso de la información recogida',
             descripcion: `Nuestro sitio web emplea la información con el fin de proporcionar el mejor servicio posible, 
             particularmente para mantener un registro de usuarios, de pedidos en caso que aplique,
             y mejorar nuestros productos y servicios.  Es posible que sean enviados correos electrónicos
@@ -20,7 +46,7 @@ module.exports = Object.freeze(
             que no exista ningún acceso no autorizado.`
         },
         {
-            titulo: 'Cookies',
+            titulo: '04. Cookies',
             descripcion: `Una cookie se refiere a un fichero que es enviado con la finalidad de solicitar permiso
             para almacenarse en su ordenador, al aceptar dicho fichero se crea y la cookie sirve entonces para tener
             información respecto al tráfico web, y también facilita las futuras real madrid cf noticias recurrente.
@@ -37,7 +63,7 @@ module.exports = Object.freeze(
             para declinar las cookies. Si se declinan es posible que no pueda utilizar algunos de nuestros servicios.`
         },
         {
-            titulo: 'Enlaces a Terceros',
+            titulo: '05. Enlaces a Terceros',
             descripcion: `Este sitio web pudiera contener en laces a otros sitios que pudieran ser de su interés.
             Una vez que usted de clic en estos enlaces y abandone nuestra página, ya no tenemos control sobre
             al sitio al que es redirigido y por lo tanto no somos responsables de los términos o privacidad
@@ -46,7 +72,7 @@ module.exports = Object.freeze(
             los consulte para confirmar que usted está de acuerdo con estas.`
         },
         {
-            titulo: 'Control de su información personal',
+            titulo: '06. Control de su información personal',
             descripcion: `En cualquier momento usted puede restringir la recopilación o el uso de la información personal
             que es proporcionada a nuestro sitio web.  Cada vez que se le solicite rellenar un formulario,
             como el de alta de usuario, puede marcar o desmarcar la opción de recibir información por correo electrónico.
