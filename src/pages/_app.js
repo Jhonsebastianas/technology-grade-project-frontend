3d1770199c76f533da7b7e4b1f13ea76aaea3765
Jhon Sebastian <Sebastamriel@gmail.com>
2021-05-19 22:14:29 -0500

    Agradecimientos y tema oscuro - 19/05/2021, 10:14 p.m.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,12 +9,15 @@ import '@styles/RegistroUsuario.css'
 import '@styles/menuPublico.css'
 import '@styles/editarCuenta.css'
 import '@styles/nosotros.css'
+import '@styles/agradecimientos.css'
 import '@styles/contactanos.css'
 import '@styles/informeConsumo.css'
 import '@styles/graficas.css'
 import '@styles/ListaHogares.css'
 import '@styles/modalEditarCuenta.css'
 import '@styles/menuPrivado.css'
+
+import '@styles/dark-theme.css'
 
 
 import { ToastProvider } from 'react-toast-notifications';
