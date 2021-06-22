0a9cdcf13b719126d82e8d371f19cf4a7d5a2735
Jhon Sebastian <Sebastamriel@gmail.com>
2021-06-21 22:07:16 -0500

    Footer - 21/06/2021, 10:07 p.m.

--- src/components/footers/FooterPublico.jsx
+++ src/components/footers/FooterPublico.jsx
@@ -5,34 +5,36 @@ import { Button, Container, Grid, Header, Icon, List, Segment } from 'semantic-u
 
 export default function FooterPublico() {
     return (
-        <Segment className="footer-bg" inverted vertical>
-            <Container>
-                <Grid divided inverted stackable>
-                    <Grid.Row>
-                        <Grid.Column width={3}>
-                            <Header inverted as='h4' content={APP_NAME} />
-                            <List link inverted>
-                                <List.Item><Link href='/nosotros'><a>Acerca de</a></Link></List.Item>
-                                <List.Item as='a'><Link href='/nosotros'><a>Contactanos</a></Link></List.Item>
-                            </List>
-                        </Grid.Column>
-                        <Grid.Column width={7}>
-                            <Header as='h4' inverted>
-                                Redes sociales
+        <footer>
+            <Segment className="footer-bg" inverted vertical>
+                <Container>
+                    <Grid divided inverted stackable>
+                        <Grid.Row>
+                            <Grid.Column width={3}>
+                                <Header inverted as='h4' content={APP_NAME} />
+                                <List link inverted>
+                                    <List.Item><Link href='/nosotros'><a>Acerca de</a></Link></List.Item>
+                                    <List.Item as='a'><Link href='/nosotros'><a>Contactanos</a></Link></List.Item>
+                                </List>
+                            </Grid.Column>
+                            <Grid.Column width={7}>
+                                <Header as='h4' inverted>
+                                    Redes sociales
                                 </Header>
-                            <p>
-                                <a rel="noreferrer" target="_blank"
-                                    href="https://www.facebook.com/Arjeware-753778911629883">
-                                    <Icon className="footer__social-icon" name="facebook square" />
-                                </a>
-                            </p>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-                <Grid centered>
-                    © 2021 - Desarrollado por {APP_NAME} | versión 0.2 (Beta)
-                </Grid>
-            </Container>
-        </Segment>
+                                <p>
+                                    <a rel="noreferrer" target="_blank"
+                                        href="https://www.facebook.com/Arjeware-753778911629883">
+                                        <Icon className="footer__social-icon" name="facebook square" />
+                                    </a>
+                                </p>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                    <Grid centered>
+                        © 2021 - Desarrollado por {APP_NAME} | versión 0.2 (Beta)
+                    </Grid>
+                </Container>
+            </Segment>
+        </footer>
     );
 }
