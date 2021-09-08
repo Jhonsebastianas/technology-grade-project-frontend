c0b46b8e8857f2891f3773fdb8e9b5f277b9c093
Jhon Sebastian <Sebastamriel@gmail.com>
2021-09-08 00:19:04 -0500

    Se adiciona service worker en home privado y se añade token - 08/09/2021, 12:19 a.m.

--- src/components/layouts/LayoutPrivado.jsx
+++ src/components/layouts/LayoutPrivado.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
-import { useRouter } from 'next/router'
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 // import Menu from '@components/menus/MenuPrivado'
-import MenuNew from '@components/menus/NewPrivateMenu'
-import loginUtils from '@utils/login.utils'
+import MenuNew from '@components/menus/NewPrivateMenu';
+import loginUtils from '@utils/login.utils';
+import ServicesWorkers from "src/tools/services.workers";
 
 const Layout = ({ children }) => {
 
@@ -12,6 +13,9 @@ const Layout = ({ children }) => {
         if (!loginUtils.verifyAuth()) {
             router.push('/login');
         }
+        if ("serviceWorker" in navigator) {
+            ServicesWorkers.configServicesWorkers();
+          }
     }, [])
 
     return (
