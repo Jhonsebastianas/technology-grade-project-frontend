346818a6697ec1fa171c0156e98714d261743e75
breynerTaborda <breinertaborda01@gmail.com>
2021-10-18 21:33:30 -0500

    18/10/2021 - 9:33PM Actualizacion menu publico, se agrega la luna y el sol con funcionalidad

--- src/components/layouts/PublicLayout.jsx
+++ src/components/layouts/PublicLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Menu from '@components/menus/NewPublicMenu'
+import Menu from '@components/menus/PublicMenu'
 import Footer from '@components/footers/FooterPublico'
 
 const Layout = ({ children }) => {
