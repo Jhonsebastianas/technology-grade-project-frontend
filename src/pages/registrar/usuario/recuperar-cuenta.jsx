a0dfcf49c100e70f839a7817aa2b572f1f1b26d6
Jhon Sebastian <Sebastamriel@gmail.com>
2021-12-26 19:16:43 -0500

    fix seo

--- src/pages/registrar/usuario/recuperar-cuenta.jsx
+++ src/pages/registrar/usuario/recuperar-cuenta.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import FormularioRecuperar from '@components/forms/usuario/RecuperarCuenta'
+import React from 'react';
+import FormularioRecuperar from '@components/forms/usuario/RecuperarCuenta';
+import BotonAtras from '@components/commons/botones/boton-atras/BotonAtras';
 
 const RecuperarCuenta = () => {
     return (
         <div className="register-photo">
+            <BotonAtras isPublic={true} />
             <div className="form-container">
                 {/* <div role="img" aria-label="imagen registro usuario" className="image-holder"></div> */}
                 <FormularioRecuperar />
